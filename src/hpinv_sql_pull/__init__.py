86d482f2357729ab2d834c4fda0cb109e943662c
austinsnyderwork <austin-snyder@uiowa.edu>
2025-10-14 13:21:28 -0500

    c

---
+++ src/hpinv_sql_pull/__init__.py
@@ -0,0 +1,1 @@
+from .sql_manager import SqlManager
---
+++ src/hpinv_sql_pull/sql_manager.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+class SqlManager:
+
+    def __init__(
+            self,
+            server: str = None,
+            database: str = None,
+            driver: str = None
+    ):
+        server = server or 'HC-SQL5'
+        database = database or 'HPINV'
+        driver = driver or 'ODBC+Driver+17+for+SQL+Server'
+
+        self.engine = create_engine(
+            f"mssql+pyodbc://{server}/{database}?driver={driver}&trusted_connection=yes"
+        )
+
+    def pull(self, sql_path: str) -> pd.DataFrame:
+        with open(sql_path, "r") as f:
+            query = f.read()
+
+        df = pd.read_sql(
+            query,
+            con=self.engine
+        )
+
+        return df
