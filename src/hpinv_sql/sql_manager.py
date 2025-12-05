404dee62048550d5da4a79ff5faf446d648cd9cb
austinsnyderwork <179244141+austinsnyderwork@users.noreply.github.com>
2025-12-05 16:17:24 -0600

    c

--- src/hpinv_sql/sql_manager.py
+++ src/hpinv_sql/sql_manager.py
@@ -13,14 +13,10 @@ class HpinvSqlManager:
 
     def __init__(
             self,
-            server: str = None,
-            database: str = None,
-            driver: str = None
+            server: str = 'HC-SQL5',
+            database: str = 'HPINV',
+            driver: str = 'ODBC+Driver+17+for+SQL+Server'
     ):
-        server = server or 'HC-SQL5'
-        database = database or 'HPINV'
-        driver = driver or 'ODBC+Driver+17+for+SQL+Server'
-
         self.engine = create_engine(
             f"mssql+pyodbc://{server}/{database}?driver={driver}&trusted_connection=yes"
         )
